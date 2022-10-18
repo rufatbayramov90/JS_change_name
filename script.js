e4b0d390365a2464cfcc5650776fcd0ff94c5c78
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-10-18 10:26:48 +0400

    Reversed_Array

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//#region  String Change Array
+let fullName = "Bayramov Rufat";
+let array = fullName.split(' ');
+let result = document.getElementById('result');
+console.log(array)
+result.innerHTML = array;
+//#endregion
 
+//#region    Array Elements Reversed
+let reversed = array.reverse();
+console.log(reversed);
+result1.innerHTML = reversed
+//#endregion
+
+//#region Array Change String
+let string = reversed.join(' ');
+console.log(string);
+result2.innerHTML = string;
+//#endregion
